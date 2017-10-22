bc6b048bd0dc90f97264937b4b62152a6e72e5c6
Rahul Prasanna <rahul.srivatsav1995@gmail.com>
2017-10-22 21:22:55 +0530

    added cli

--- image_restore/denoise.py
+++ image_restore/denoise.py
@@ -1,9 +1,23 @@
 import numpy as np
 import cv2
+import os
+import argparse
 from matplotlib import pyplot as plt
-img = cv2.imread('imgs/noise.jpg')
+
+parser = argparse.ArgumentParser(description = "a basic denoiser which uses OpenCV")
+parser.add_argument('--input', required = True, type = str, help = "path to the image to be denoised")
+parser.add_argument('--output', required = True, type = str, help = 'name to be given to the output image')
+args = parser.parse_args()
+
+
+input_img = args.input
+img = cv2.imread(input_img)
+
 dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-cv2.imwrite('noised.jpg', dst)
+
+result = args.output
+
+cv2.imwrite(result, dst)
 plt.subplot(121),plt.imshow(img)
 plt.subplot(122),plt.imshow(dst)
 plt.show()
